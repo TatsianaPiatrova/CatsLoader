10122a18293adab0af215f304167f16b0867711f
Tanya <53278501+TatsianaPiatrova@users.noreply.github.com>
2021-09-29 21:24:50 +0300

    add: load button

--- app/src/main/java/com/example/catsloader/MainActivity.kt
+++ app/src/main/java/com/example/catsloader/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.catsloader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View.inflate
+import androidx.core.graphics.drawable.DrawableCompat.inflate
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.catsloader.databinding.MainActivityBinding
+import com.example.catsloader.databinding.MainActivityBinding.inflate
 import com.example.catsloader.ui.main.MainViewModel
 import com.example.catsloader.ui.main.MainViewModelFactory
 
@@ -14,15 +18,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        val binding = inflate(layoutInflater)
+        setContentView(binding.root)
 
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
-        viewModel.getCat()
-        viewModel.myResponse.observe(this, Observer { response ->
-            Log.d("Response", response[0].id)
-            Log.d("Response", response[0].url)
-        })
+
+        binding.button.setOnClickListener{
+            viewModel.getCat()
+            viewModel.myResponse.observe(this, Observer { response ->
+                binding.textView.text = response[0].id
+                binding.textView2.text = response[0].url
+            })
+        }
     }
 }
